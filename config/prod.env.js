a83037ef144ef5ea6081299b92f5e915d32fad63
Unknown <gregoire.hage@gmail.com>
2020-05-19 01:33:08 +0200

    call prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  externals: {
+    jquery: 'jQuery',
+    bootstrap: 'bootstrap',
+    popper: 'popper.js'
+  }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'popper.js'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
