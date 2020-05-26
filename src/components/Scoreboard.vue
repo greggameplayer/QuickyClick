b3bf767221a6c09db45fb8aab1cdf8ecfffcaec3
Unknown <gregoire.hage@gmail.com>
2020-05-26 20:07:51 +0200

    implement scoreboards

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,58 +1,136 @@
 <template>
     <div v-if="finished" class="scoreboard mt-5">
       <h1 class="text-center display-4">Scoreboard</h1>
-      <table class="table">
-        <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Player</th>
-          <th scope="col">Score</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="(item, index) in playersArray" :key="item.id">
-          <th scope="row">{{ index + 1 }}</th>
-          <td>{{ item.name }}</td>
-          <td>{{ item.score }}</td>
-        </tr>
-        </tbody>
-      </table>
+      <div id="scoreboardcontainer" class="text-center">
+        <div class="btn-group-vertical">
+            <button class="btn btn-primary" @click="onClickLocal" :class="{active: btclicked === 0}">Local</button>
+            <button class="btn btn-primary" @click="onClickOnline" :class="{active: btclicked === 1}">Online</button>
+        </div>
+        <table class="table" v-if="btclicked === 0">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Player</th>
+              <th scope="col">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in playersArray" :key="item.id">
+              <th scope="row" :class="{gold: item.rank === 1, silver: item.rank === 2, bronze: item.rank === 3}">{{ item.rank }}</th>
+              <td>{{ item.name }}</td>
+              <td>{{ item.score }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="table" v-if="btclicked === 1">
+          <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Player</th>
+            <th scope="col">Score</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in onlineScores" :key="item.id">
+            <th scope="row" :class="{gold: item.rank === 1, silver: item.rank === 2, bronze: item.rank === 3}">{{ item.rank }}</th>
+            <td>{{ item.name }}</td>
+            <td>{{ item.score }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
 </template>
 
 <script>
+import { db } from '../db'
+
 export default {
   name: 'Scoreboard',
   data: function () {
     return {
-      finished: false
+      finished: false,
+      btclicked: 0,
+      onlineScores: [],
+      LocalRank: 1,
+      OnlineRank: 1
     }
+  },
+  firestore: {
+    onlineScores: db.collection('onlineScores')
   },
   props: ['playerName', 'score', 'time', 'playersArray'],
   watch: {
-    time: function () {
+    time: async function () {
       if (!this.time) {
         const self = this
-        let modifyScore = false
-        setTimeout(function () {
+        setTimeout(async function () {
           for (let i = 0; i < self.playersArray.length; i++) {
             if (self.playerName === self.playersArray[i].name) {
-              self.playersArray[i].score = self.score
+              if (self.score > self.playersArray[i].score) {
+                self.playersArray[i].score = self.score
+              }
               localStorage.setItem('players', JSON.stringify(self.playersArray))
-              modifyScore = true
+              await db.collection('onlineScores').doc(self.playerName).set({
+                name: self.playersArray[i].name,
+                score: self.playersArray[i].score
+              }).then(() => {
+                console.log('user updated!')
+              })
             }
-          }
-
-          if (modifyScore === false) {
-            self.playersArray[this.playersArray.length - 1].score = self.score
-            localStorage.setItem('players', JSON.stringify(self.playersArray))
           }
           self.playersArray.sort(function (a, b) {
             return b.score - a.score
           })
+
+          for (let i = 0; i < self.playersArray.length; i++) {
+            if (typeof self.playersArray[i + 1] !== 'undefined') {
+              if (self.playersArray[i].score === self.playersArray[i + 1].score) {
+                Object.assign(self.playersArray[i], { rank: self.LocalRank })
+                Object.assign(self.playersArray[i + 1], { rank: self.LocalRank })
+              } else {
+                Object.assign(self.playersArray[i], { rank: self.LocalRank })
+                self.LocalRank++
+                Object.assign(self.playersArray[i + 1], { rank: self.LocalRank })
+              }
+            } else {
+              if (self.playersArray[i] === self.playersArray[0]) {
+                Object.assign(self.playersArray[i], { rank: self.LocalRank })
+              }
+            }
+          }
+
           self.finished = true
+          self.onlineScores.sort(function (a, b) {
+            return b.score - a.score
+          })
+
+          for (let i = 0; i < self.onlineScores.length; i++) {
+            if (typeof self.onlineScores[i + 1] !== 'undefined') {
+              if (self.onlineScores[i].score === self.onlineScores[i + 1].score) {
+                Object.assign(self.onlineScores[i], { rank: self.OnlineRank })
+                Object.assign(self.onlineScores[i + 1], { rank: self.OnlineRank })
+              } else {
+                Object.assign(self.onlineScores[i], { rank: self.OnlineRank })
+                self.OnlineRank++
+                Object.assign(self.onlineScores[i + 1], { rank: self.OnlineRank })
+              }
+            } else {
+              if (self.onlineScores[i] === self.onlineScores[0]) {
+                Object.assign(self.onlineScores[i], { rank: self.OnlineRank })
+              }
+            }
+          }
         }, 1000)
       }
+    }
+  },
+  methods: {
+    onClickLocal: function () {
+      this.btclicked = 0
+    },
+    onClickOnline: function () {
+      this.btclicked = 1
     }
   }
 }
@@ -73,5 +151,30 @@ export default {
     display: flex;
     flex-direction: column;
     width: 100vw;
+  }
+
+  #scoreboardcontainer
+  {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .btn-group-vertical {
+    max-width: 30%;
+    height: 5%;
+  }
+
+  .bronze {
+    color: saddlebrown;
+  }
+
+  .silver {
+    color: silver;
+  }
+
+  .gold {
+    color: gold;
   }
 </style>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,18 +1,41 @@
 <template>
-  <div>
-    <span :class="{ hide: !player }">{{ welcomeMessage }}</span>
-    <form class="form-inline mt-3" v-on:submit.prevent="setPlayer" :class="{ hide: playerName }">
-      <div class="input-group mb-2">
-        <input class="form-control" name="player" placeholder="Entrer votre nom de joueur">
-        <div class="input-group-append">
-          <button class="btn btn-primary" type="submit">Jouer</button>
+  <div id="usercontainer">
+    <span :class="{ hide: !player }" id="welcomeMsg">{{ welcomeMessage }}</span>
+    <div id="userformcontainer">
+      <form id="userform">
+        <div id="fieldscontainer" :class="{hide: logged || !renderAuth}" class="form-group">
+          <input class="form-control mb-2" type="email" name="email" id="txtEmail" v-model="authInput.txtEmail" placeholder="Email" required>
+          <input class="form-control" type="Password" name="password" id="txtPassword" v-model="authInput.txtPassword" placeholder="Password" required>
         </div>
-      </div>
-    </form>
+        <div id="connexioncontainer" class="form-group flex-row align-items-between mb-2">
+        <button type="submit" id="btnLogin" v-on:submit.prevent="Login()" :class="{hide: logged || !renderAuth}" class="btn btn-primary mx-3 my-2">
+          Log in
+        </button>
+        <button type="submit" id="btnSignUp" v-on:submit.prevent="SignUp()" :class="{hide: logged || !renderAuth}"  class="btn btn-primary mx-3 my-2">
+          Sign Up
+        </button>
+        <button id="btnLogout" v-on:click="LogOut()" :class="{hide: !logged || !renderAuth}" class="btn btn-primary">
+          Log out
+        </button>
+        <button type="button" v-on:click="googleLogin()" :class="{hide: logged || !renderAuth}" class="btn btn-primary mx-3 my-2" id="googleIn">
+          Google in
+        </button>
+        </div>
+        <div>
+          <button v-on:click="play()" class="btn btn-primary" id="btnplay">
+            Play
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase/app'
+// eslint-disable-next-line no-unused-vars
+import { db } from '../db'
+var provider = new Firebase.auth.GoogleAuthProvider()
 export default {
   name: 'user',
   props: ['playerName'],
@@ -21,7 +44,13 @@ export default {
       player: this.playerName,
       players: [],
       welcomeMessage: '',
-      playerId: 0
+      playerId: 0,
+      logged: false,
+      renderAuth: false,
+      authInput: {
+        txtEmail: '',
+        txtPassword: ''
+      }
     }
   },
   created: function () {
@@ -31,22 +60,13 @@ export default {
     } else {
       localStorage.setItem('players', JSON.stringify(this.players))
     }
-  },
-  updated: function () {
-    this.welcomeMessage = `Bonjour ${this.player} !`
+    Firebase.auth().onAuthStateChanged(this.authStateChange)
   },
   methods: {
-    setPlayer: function (event) {
-      const playerName = event.target[0].value
-
-      if (!playerName) {
-        window.alert('Merci de renseigner votre nom')
-        return
-      }
-
+    setPlayer: function (playerName) {
       this.player = playerName
-      this.$emit('player')
       this.$emit('playerName', playerName)
+      this.welcomeMessage = `Bonjour ${this.player} !`
 
       for (let i = 0; i < this.players.length; i++) {
         if (playerName === this.players[i].name) {
@@ -54,9 +74,74 @@ export default {
           return
         }
       }
-      this.players.push({ name: playerName, id: this.playerId, score: 0 })
+      this.players.push({ name: playerName, id: this.playerId, score: 0, rank: 0 })
       this.$emit('playersArray', this.players)
       this.playerId++
+    },
+    Login: function (event) {
+      const email = this.authInput.txtEmail
+      const pass = this.authInput.txtPassword
+      const auth = Firebase.auth()
+      const promise = auth.signInWithEmailAndPassword(email, pass)
+      this.authInput.txtEmail = ''
+      this.authInput.txtPassword = ''
+      promise.catch(event => console.log(event.message))
+    },
+    SignUp: function (event) {
+      const email = this.authInput.txtEmail
+      const pass = this.authInput.txtPassword
+      const auth = Firebase.auth()
+      const promise = auth.createUserWithEmailAndPassword(email, pass)
+      this.authInput.txtEmail = ''
+      this.authInput.txtPassword = ''
+      promise.catch(event => console.log(event.message))
+    },
+    googleLogin: function () {
+      Firebase.auth().signInWithPopup(provider).then(function (result) {
+        console.log(result)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    },
+    LogOut: function () {
+      Firebase.auth().signOut()
+    },
+    authStateChange: function (firebaseUser) {
+      if (firebaseUser) {
+        firebaseUser.sendEmailVerification().then(function () {
+          console.log('send Verification')
+        }, function (_error) {
+          console.log('not send Verification')
+        })
+        if (firebaseUser.emailVerified === true) {
+          console.log('login success')
+          this.logged = true
+          if (firebaseUser.displayName) {
+            this.setPlayer(firebaseUser.displayName)
+          } else {
+            this.setPlayer(firebaseUser.email)
+          }
+        } else {
+          this.logged = false
+          this.welcomeMessage = ''
+        }
+      } else {
+        console.log('not loggend in')
+        this.logged = false
+        this.welcomeMessage = ''
+      }
+
+      if (this.renderAuth === false) {
+        this.renderAuth = true
+      }
+    },
+    play: function () {
+      if (!this.player) {
+        window.alert('Merci de vous connecter')
+        return
+      }
+      this.$emit('player')
+      document.querySelector('#btnplay').setAttribute('disabled', true)
     }
   }
 }
@@ -67,10 +152,32 @@ export default {
     display: none;
   }
 
-  div > form {
+  #usercontainer {
+    display: block;
+    width: 100%;
+  }
+
+  #welcomeMsg {
+    text-align: center;
+    display: block;
+  }
+
+  #userform
+  {
     display: flex;
     flex-direction: column;
-    width: 100%;
     align-items: center;
+  }
+
+  #btnplay {
+    margin-bottom: 1vh;
+  }
+
+  #userform {
+    margin-top: 1vh;
+  }
+
+  button {
+    width: 6rem;
   }
 </style>
