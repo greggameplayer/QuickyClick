70eb77618ae9d673b5b5c17203866d165562f04d
Unknown <gregoire.hage@gmail.com>
2020-05-27 01:54:00 +0200

    add icons and delete unused files

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/QuickyClick/'
-    : '/'
+    : '/',
+  pwa: {
+    themeColor: '#17a2b8',
+    msTileColor: '#da532c',
+    iconPaths: {
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
+  }
 }
