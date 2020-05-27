595c5e7645f8c917c682878b045499d47378408c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-27 13:22:40 +0000

    Deploy greggameplayer/QuickyClick to github.com/greggameplayer/QuickyClick.git:gh-pages

---
+++ precache-manifest.edb244a95e6a71bf5df9e8776d765b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fda24e6456fd51e383a",
+    "url": "/QuickyClick/css/app.4742c201.css"
+  },
+  {
+    "revision": "eb8c761610625a3b730b",
+    "url": "/QuickyClick/css/chunk-vendors.954f8d1b.css"
+  },
+  {
+    "revision": "806b1a13dd6ed27956770f3b17eede09",
+    "url": "/QuickyClick/index.html"
+  },
+  {
+    "revision": "7fda24e6456fd51e383a",
+    "url": "/QuickyClick/js/app.07224acf.js"
+  },
+  {
+    "revision": "eb8c761610625a3b730b",
+    "url": "/QuickyClick/js/chunk-vendors.2b98e53a.js"
+  },
+  {
+    "revision": "9ffe1a08ba774298d5c9cf85317401ec",
+    "url": "/QuickyClick/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/QuickyClick/robots.txt"
+  }
+]);
