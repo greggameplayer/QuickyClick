f7a8bf5653200e64aeff648716f136f87fe3c4d0
Unknown <gregoire.hage@gmail.com>
2020-05-27 02:15:44 +0200

    correct icons path

--- vue.config.js
+++ vue.config.js
@@ -7,11 +7,11 @@ module.exports = {
     themeColor: '#17a2b8',
     msTileColor: '#da532c',
     iconPaths: {
-      msTileImage: 'img/icons/mstile-150x150.png',
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg'
+      msTileImage: './img/icons/mstile-150x150.png',
+      favicon32: './img/icons/favicon-32x32.png',
+      favicon16: './img/icons/favicon-16x16.png',
+      appleTouchIcon: './img/icons/apple-touch-icon.png',
+      maskIcon: './img/icons/safari-pinned-tab.svg'
     }
   }
 }
