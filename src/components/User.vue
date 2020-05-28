9eb9c381e80132fcaa544e9151a52166d60904af
Unknown <gregoire.hage@gmail.com>
2020-05-29 01:58:43 +0200

    Auth system refactor
    - correct auth by email/password
    - redesign auth system
    - add alert when error or sending verification mail

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="usercontainer">
     <span :class="{ hide: !player }" id="welcomeMsg">{{ welcomeMessage }}</span>
-    <div id="userformcontainer">
-      <form id="userform">
-        <div id="fieldscontainer" :class="{hide: logged || !renderAuth}" class="form-group">
-          <input class="form-control mb-2" type="email" name="email" id="txtEmail" v-model="authInput.txtEmail" placeholder="Email" required>
-          <input class="form-control" type="Password" name="password" id="txtPassword" v-model="authInput.txtPassword" placeholder="Password" required>
-        </div>
-        <div id="connexioncontainer" class="form-group flex-row align-items-between mb-2">
-        <button type="submit" id="btnLogin" v-on:submit.prevent="Login()" :class="{hide: logged || !renderAuth}" class="btn btn-primary mx-3 my-2">
-          Log in
-        </button>
-        <button type="submit" id="btnSignUp" v-on:submit.prevent="SignUp()" :class="{hide: logged || !renderAuth}"  class="btn btn-primary mx-3 my-2">
-          Sign Up
-        </button>
-        <button id="btnLogout" v-on:click="LogOut()" :class="{hide: !logged || !renderAuth}" class="btn btn-primary">
+    <div id="userauthglobalcontainer">
+      <div class="btn-group mt-2">
+        <button class="btn btn-primary" @click="onClickLoginBT" :class="{active: connexiontab === 0}">Log in</button>
+        <button class="btn btn-primary" @click="onClickSignUpBT" :class="{active: connexiontab === 1}">Sign Up</button>
+      </div>
+      <div id="userformcontainer" class="border-primary border rounded shadow">
+        <form id="userformlogin" v-on:submit.prevent="Login()" v-if="connexiontab === 0 && !logged">
+          <div id="fieldscontainer" :class="{hide: logged || !renderAuth}" class="form-group">
+            <input class="form-control mb-2" type="email" name="email" id="txtEmail" v-model="authInput.txtEmail" placeholder="Email" required>
+            <input class="form-control" type="Password" name="password" id="txtPassword" v-model="authInput.txtPassword" placeholder="Password" required>
+          </div>
+          <div id="connexioncontainer" class="form-group flex-row align-items-between mb-2">
+            <button type="submit" id="btnLogin" :class="{hide: logged || !renderAuth}" class="btn btn-primary mx-3 my-2">
+              Log in
+            </button>
+            <button type="button" v-on:click="googleLogin()" :class="{hide: logged || !renderAuth}" class="btn btn-primary mx-3 my-2" id="googleIn">
+              Google in
+            </button>
+          </div>
+        </form>
+        <form id="userformsignup" v-on:submit.prevent="SignUp()" v-if="connexiontab === 1 && !logged">
+          <div id="fieldscontainersignup" :class="{hide: logged || !renderAuth}" class="form-group">
+            <input class="form-control mb-2" type="email" name="email" id="txtEmailsignup" v-model="authInput.txtEmail" placeholder="Email" required>
+            <input class="form-control" type="Password" name="password" id="txtPasswordsignup" v-model="authInput.txtPassword" placeholder="Password" required>
+          </div>
+          <div id="signupcontainer" class="form-group flex-row align-items-between mb-2">
+            <button type="submit" id="btnSignUp" :class="{hide: logged || !renderAuth}"  class="btn btn-primary mx-3 my-2">
+              Sign Up
+            </button>
+          </div>
+        </form>
+        <button id="btnLogout" v-on:click="LogOut()" :class="{hide: !logged || !renderAuth}" class="btn btn-primary mb-2 mt-2">
           Log out
         </button>
-        <button type="button" v-on:click="googleLogin()" :class="{hide: logged || !renderAuth}" class="btn btn-primary mx-3 my-2" id="googleIn">
-          Google in
-        </button>
-        </div>
         <div>
-          <button v-on:click="play()" :class="{hide: !renderAuth}" class="btn btn-primary" id="btnplay">
+          <button v-on:click="play()" :class="{hide: !renderAuth}" class="btn btn-primary mb-2" id="btnplay">
             Play
           </button>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +59,7 @@ export default {
       players: [],
       welcomeMessage: '',
       playerId: 0,
+      connexiontab: 0,
       logged: false,
       renderAuth: false,
       authInput: {
@@ -63,6 +78,12 @@ export default {
     Firebase.auth().onAuthStateChanged(this.authStateChange)
   },
   methods: {
+    onClickLoginBT: function (_event) {
+      this.connexiontab = 0
+    },
+    onClickSignUpBT: function (_event) {
+      this.connexiontab = 1
+    },
     setPlayer: function (playerName) {
       this.player = playerName
       this.$emit('playerName', playerName)
@@ -77,6 +98,9 @@ export default {
       this.players.push({ name: playerName, id: this.playerId, score: 0, rank: 0 })
       this.$emit('playersArray', this.players)
       this.playerId++
+    },
+    userformSubmit: function (event) {
+      console.log(event)
     },
     Login: function (event) {
       const email = this.authInput.txtEmail
@@ -107,13 +131,17 @@ export default {
       Firebase.auth().signOut()
     },
     authStateChange: function (firebaseUser) {
+      const self = this
       if (firebaseUser) {
-        firebaseUser.sendEmailVerification().then(function () {
-          console.log('send Verification')
-        }, function (_error) {
-          console.log('not send Verification')
-        })
-        if (firebaseUser.emailVerified === true) {
+        if (!firebaseUser.emailVerified) {
+          firebaseUser.sendEmailVerification().then(function () {
+            self.$alert('You\'ve received a verification email, please click on the link in it to be able to login')
+          }, function (_error) {
+            self.$alert('Verification email failed to be sent, please try again')
+          })
+          this.logged = false
+          this.welcomeMessage = ''
+        } else {
           console.log('login success')
           this.logged = true
           if (firebaseUser.displayName) {
@@ -121,9 +149,6 @@ export default {
           } else {
             this.setPlayer(firebaseUser.email)
           }
-        } else {
-          this.logged = false
-          this.welcomeMessage = ''
         }
       } else {
         console.log('not loggend in')
@@ -162,22 +187,35 @@ export default {
     display: block;
   }
 
-  #userform
+  #userformlogin, #userformsignup
   {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
 
-  #btnplay {
-    margin-bottom: 1vh;
-  }
-
-  #userform {
+  #userformlogin, #userformsignup {
     margin-top: 1vh;
   }
 
   button {
     width: 6rem;
+  }
+
+  .btn-group-vertical {
+    max-width: 30%;
+    height: 5%;
+  }
+
+  #userauthglobalcontainer
+  {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+
+  #userformcontainer {
+    width: 16.5rem;
   }
 </style>
