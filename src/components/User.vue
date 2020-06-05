99e0154ca18bacb09882e91ce63f7553e41d5b9a
Unknown <gregoire.hage@gmail.com>
2020-06-06 01:46:09 +0200

    refactor user #2
    - fallback user of all possible errors during the login/signup
    - ask a displayName on signup
    - correct direct login after signup

--- src/components/User.vue
+++ src/components/User.vue
@@ -23,6 +23,7 @@
         </form>
         <form id="userformsignup" v-on:submit.prevent="SignUp()" v-if="connexiontab === 1 && !logged">
           <div id="fieldscontainersignup" :class="{hide: logged || !renderAuth}" class="form-group">
+            <input class="form-control mb-2" type="text" name="name" id="txtNamesignup" v-model="authInput.txtName" placeholder="Name" required>
             <input class="form-control mb-2" type="email" name="email" id="txtEmailsignup" v-model="authInput.txtEmail" placeholder="Email" required>
             <input class="form-control" type="Password" name="password" id="txtPasswordsignup" v-model="authInput.txtPassword" placeholder="Password" required>
           </div>
@@ -61,10 +62,12 @@ export default {
       playerId: 0,
       connexiontab: 0,
       logged: false,
+      regularLogin: false,
       renderAuth: false,
       authInput: {
         txtEmail: '',
-        txtPassword: ''
+        txtPassword: '',
+        txtName: ''
       }
     }
   },
@@ -103,22 +106,88 @@ export default {
       console.log(event)
     },
     Login: function (event) {
+      const self = this
+      this.regularLogin = true
       const email = this.authInput.txtEmail
       const pass = this.authInput.txtPassword
       const auth = Firebase.auth()
       const promise = auth.signInWithEmailAndPassword(email, pass)
+      promise.catch(function (event) {
+        console.log(event.message)
+        switch (event.code) {
+          case 'auth/user-not-found':
+            self.$alert('The user with the credentials used doesn\'t exist.')
+            break
+          case 'auth/wrong-password':
+            self.$alert('The password you\'ve entered for this user is wrong.')
+            break
+          case 'auth/too-many-requests':
+            self.$alert('Too many request, please try again later.')
+            break
+          case 'auth/unauthorized-domain':
+            self.$alert('Unauthorized domain, please try again later.')
+            break
+          case 'auth/user-disabled':
+            self.$alert('This user has been disabled by the administrator.')
+            break
+          case 'auth/user-token-expired':
+            self.$alert('The credentials of this user has expired. It might be because this user has been deleted.')
+            break
+        }
+      })
+      promise.then(function (result) {
+        console.log('login success')
+        self.logged = true
+        if (result.user.displayName) {
+          self.setPlayer(result.user.displayName)
+        } else {
+          self.setPlayer(result.user.email)
+        }
+      })
       this.authInput.txtEmail = ''
       this.authInput.txtPassword = ''
-      promise.catch(event => console.log(event.message))
     },
     SignUp: function (event) {
+      const self = this
+      const name = this.authInput.txtName
       const email = this.authInput.txtEmail
       const pass = this.authInput.txtPassword
       const auth = Firebase.auth()
       const promise = auth.createUserWithEmailAndPassword(email, pass)
+      promise.then(function (result) {
+        result.user.updateProfile({
+          displayName: name
+        }).then(function () {
+          console.log('displayName update successful')
+        }).catch(function (error) {
+          console.log('displayName update error : ' + error.message)
+        })
+      })
       this.authInput.txtEmail = ''
       this.authInput.txtPassword = ''
-      promise.catch(event => console.log(event.message))
+      this.authInput.txtName = ''
+      promise.catch(function (event) {
+        console.log(event.message)
+        switch (event.code) {
+          case 'auth/email-already-exists':
+          case 'auth/email-already-in-use':
+            self.$alert('The email entered is already used in another user account.')
+            break
+          case 'auth/internal-error':
+            self.$alert('The authentication server has encountered an unexpected error, please try again later.')
+            break
+          case 'auth/invalid-display-name':
+            self.$alert('The name field have to be fill.')
+            break
+          case 'auth/invalid-email':
+            self.$alert('The provided value inside the email field isn`t valid.')
+            break
+          case 'auth/invalid-password':
+          case 'auth/weak-password':
+            self.$alert('The password entered is invalid, it must contain at least 6 characters.')
+            break
+        }
+      })
     },
     googleLogin: function () {
       Firebase.auth().signInWithPopup(provider).then(function (result) {
@@ -128,41 +197,44 @@ export default {
       })
     },
     LogOut: function () {
+      this.regularLogin = false
       Firebase.auth().signOut()
     },
     authStateChange: function (firebaseUser) {
       const self = this
-      if (firebaseUser) {
-        if (!firebaseUser.emailVerified) {
-          firebaseUser.sendEmailVerification().then(function () {
-            self.$alert('You\'ve received a verification email, please click on the link in it to be able to login')
-          }, function (_error) {
-            self.$alert('Verification email failed to be sent, please try again')
-          })
+      if (!self.regularLogin) {
+        if (firebaseUser) {
+          if (!firebaseUser.emailVerified) {
+            firebaseUser.sendEmailVerification().then(function () {
+              self.$alert('You\'ve received a verification email, please click on the link in it to be able to login. If you doesn\'t find it in your regular inbox please check your spam.')
+            }, function (_error) {
+              self.$alert('Verification email failed to be sent, please try again.')
+            })
+            this.logged = false
+            this.welcomeMessage = ''
+          } else {
+            console.log('login success')
+            this.logged = true
+            if (firebaseUser.displayName) {
+              this.setPlayer(firebaseUser.displayName)
+            } else {
+              this.setPlayer(firebaseUser.email)
+            }
+          }
+        } else {
+          console.log('not loggend in')
           this.logged = false
           this.welcomeMessage = ''
-        } else {
-          console.log('login success')
-          this.logged = true
-          if (firebaseUser.displayName) {
-            this.setPlayer(firebaseUser.displayName)
-          } else {
-            this.setPlayer(firebaseUser.email)
-          }
         }
-      } else {
-        console.log('not loggend in')
-        this.logged = false
-        this.welcomeMessage = ''
-      }
 
-      if (this.renderAuth === false) {
-        this.renderAuth = true
+        if (this.renderAuth === false) {
+          this.renderAuth = true
+        }
       }
     },
     play: function () {
       if (!this.player) {
-        window.alert('Please connect yourself')
+        self.$alert('Please connect yourself.')
         return
       }
       this.$emit('player')
